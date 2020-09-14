2162621a19c75edfb705f5b440914f833cf5a028
noah0316 <noa316@naver.com>
2020-09-15 00:04:38 +0900

    Using sub-components sub-components is a  break the UI into reusable pieces

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,11 @@
 import React from "react"
+import Header from "../components/header"
 
 export default function About() {
   return (
     <div style={{ color: `teal` }}>
-      <h1>About Gatsby</h1>
+      <Header headerText="About Gatsby" arbitraryPhrase="is arbitrary" />
+      <Header headerText="It's pretty cool" />
       <p>Such wow. Very React.</p>
     </div>
   )

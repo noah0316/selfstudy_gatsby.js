c22bcc181edf95a83e1ce16e72663a13def16e90
noah0316 <noa316@naver.com>
2020-09-15 23:33:23 +0900

    Using the <Link /> component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,15 @@
 import React from "react"
+import { Link } from "gatsby"
 
 export default function Home() {
   return (
-    <div style={{ color: `purple` }}>
-      <h1>Hello world!</h1>
-      <p> What a world.</p>
-      <img src="https://source.unsplash.com/random/400x200" alt="" />
-    </div>
+    <>
+      <Link to="/contact">Contact</Link>
+      <div style={{ color: `purple` }}>
+        <h1>Hello world!</h1>
+        <p> What a world.</p>
+        <img src="https://source.unsplash.com/random/400x200" alt="" />
+      </div>
+    </>
   )
 }

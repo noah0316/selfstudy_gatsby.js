5535723da860800d21c7d2f1c9191881e536f8c8
noah0316 <noa316@naver.com>
2020-09-16 23:02:02 +0900

    2. Introduction to Styling in Gatsby

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,8 @@
 import React from "react"
-import { Link } from "gatsby"
 
 export default function Home() {
   return (
     <div style={{ color: `purple` }}>
-      <Link to="/contact">Contact</Link>
       <h1>Hello world!</h1>
       <p> What a world.</p>
       <img src="https://source.unsplash.com/random/400x200" alt="" />

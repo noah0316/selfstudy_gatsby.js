3f21cccdcda8da0b1d642bdcb836ca06d20453e9
noah0316 <noa316@naver.com>
2020-09-18 01:17:22 +0900

    3. Creating Layout Components

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,11 +1,14 @@
 import React from "react"
+import Layout from "../components/layout"
 export default function Contact() {
   return (
-    <div>
-      <h1>I'd love to talk! Email me at the address below</h1>
-      <p>
-        <a href="noa316@naver.com">me@example.com</a>
-      </p>
-    </div>
+    <Layout>
+      <div>
+        <h1>I'd love to talk! Email me at the address below</h1>
+        <p>
+          <a href="noa316@naver.com">me@example.com</a>
+        </p>
+      </div>
+    </Layout>
   )
 }

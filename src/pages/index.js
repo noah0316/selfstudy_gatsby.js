a387fccffecccb2b58e575817f4c3868023b0a7d
noah0316 <noa316@naver.com>
2020-09-17 23:55:09 +0900

    3. Creating Nested Layout Components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,12 @@
 import React from "react"
-
 export default function Home() {
   return (
-    <div style={{ color: `purple` }}>
-      <h1>Hello world!</h1>
-      <p> What a world.</p>
-      <img src="https://source.unsplash.com/random/400x200" alt="" />
+    <div style={{ margin: `3rem auto`, maxWidth: 600 }}>
+      <h1>Hi! I'm building a fake Gatsby site as part of a tutorial!</h1>
+      <p>
+        What do I like to do? Lots of course but definitely enjoy building
+        websites.
+      </p>
     </div>
   )
 }
